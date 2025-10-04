569d1dedee12f79ea9d76984138373e3110cbb12
Fly Dev <dev@fly.io>
2025-10-04 15:05:45 +0000

    add show_modal and hide_modal JS helpers

--- lib/mykonos_biennale_web/components/core_components.ex
+++ lib/mykonos_biennale_web/components/core_components.ex
@@ -505,6 +505,30 @@ defmodule MykonosBiennaleWeb.CoreComponents do
     )
   end
 
+  def show_modal(js \\ %JS{}, id) when is_binary(id) do
+    js
+    |> JS.show(to: "##{id}")
+    |> JS.show(
+      to: "##{id}-bg",
+      time: 300,
+      transition: {"transition-all ease-out duration-300", "opacity-0", "opacity-100"}
+    )
+    |> show("##{id}-container")
+    |> JS.add_class("overflow-hidden", to: "body")
+    |> JS.focus_first(to: "##{id}-content")
+  end
+
+  def hide_modal(js \\ %JS{}, id) do
+    js
+    |> JS.hide(
+      to: "##{id}-bg",
+      transition: {"transition-all ease-in duration-200", "opacity-100", "opacity-0"}
+    )
+    |> hide("##{id}-container")
+    |> JS.hide(to: "##{id}", transition: {"block", "block", "hidden"})
+    |> JS.remove_class("overflow-hidden", to: "body")
+    |> JS.pop_focus()
+
   def hide(js \\ %JS{}, selector) do
     JS.hide(js,
       to: selector,
@@ -513,7 +537,6 @@ defmodule MykonosBiennaleWeb.CoreComponents do
         {"transition-all ease-in duration-200", "opacity-100 translate-y-0 sm:scale-100",
          "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"}
     )
-  end
 
   @doc """
   Translates an error message using gettext.
@@ -531,12 +554,9 @@ defmodule MykonosBiennaleWeb.CoreComponents do
     Enum.reduce(opts, msg, fn {key, value}, acc ->
       String.replace(acc, "%{#{key}}", fn _ -> to_string(value) end)
     end)
-  end
 
   @doc """
   Translates the errors for a field from a keyword list of errors.
   """
   def translate_errors(errors, field) when is_list(errors) do
     for {^field, {msg, opts}} <- errors, do: translate_error({msg, opts})
-  end
-end
