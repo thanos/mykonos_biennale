349d4f551d8578ac0a8070a6d99b800e22296446
Fly Dev <dev@fly.io>
2025-10-04 15:04:53 +0000

    add admin biennale routes

--- lib/mykonos_biennale_web/router.ex
+++ lib/mykonos_biennale_web/router.ex
@@ -55,6 +55,9 @@ defmodule MykonosBiennaleWeb.Router do
     live_session :require_authenticated_user,
       on_mount: [{MykonosBiennaleWeb.UserAuth, :require_authenticated}] do
       live "/admin", Admin.DashboardLive
+      live "/admin/biennales", Admin.BiennaleLive.Index, :index
+      live "/admin/biennales/new", Admin.BiennaleLive.Index, :new
+      live "/admin/biennales/:id/edit", Admin.BiennaleLive.Index, :edit
       live "/users/settings", UserLive.Settings, :edit
       live "/users/settings/confirm-email/:token", UserLive.Settings, :confirm_email
     end
