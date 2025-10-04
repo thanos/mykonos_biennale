707710574b290e6067972912330e82d9bd959015
Fly Dev <dev@fly.io>
2025-10-04 14:55:26 +0000

    add admin dashboard route

--- lib/mykonos_biennale_web/router.ex
+++ lib/mykonos_biennale_web/router.ex
@@ -54,6 +54,7 @@ defmodule MykonosBiennaleWeb.Router do
 
     live_session :require_authenticated_user,
       on_mount: [{MykonosBiennaleWeb.UserAuth, :require_authenticated}] do
+      live "/admin", Admin.DashboardLive
       live "/users/settings", UserLive.Settings, :edit
       live "/users/settings/confirm-email/:token", UserLive.Settings, :confirm_email
     end
