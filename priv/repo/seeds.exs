7a5087296ed7279e6168e6e68fe3c420e29db66b
Fly Dev <dev@fly.io>
2025-10-04 14:29:26 +0000

    seed default admin user

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,14 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias MykonosBiennale.Accounts
+
+# Create default admin user
+{:ok, admin} =
+  Accounts.register_user(%{email: "admin@mykonosbiennale.com", password: "adminpassword123"})
+
+{:ok, _admin, _expired_tokens} =
+  Accounts.update_user_password(admin, "adminpassword123", %{password: "adminpassword123"})
+
+IO.puts("✓ Created admin user: admin@mykonosbiennale.com / adminpassword123")
